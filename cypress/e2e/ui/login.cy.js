708977e7b1172613e07e407d6848939e631568b4
marcusautomatic <149791208+marcusautomatic@users.noreply.github.com>
2023-11-08 14:07:46 +0100

    update login & added new command

--- cypress/e2e/ui/login.cy.js
+++ cypress/e2e/ui/login.cy.js
@@ -4,16 +4,14 @@
 describe('User Sign-up and Login', () => {
   beforeEach(() => {
     
+    // Given I am on the login page
     cy.visit('/signin/')
   
   })
 
-  it('should allow a visitor to sign-up, login, and logout', () => {
-    cy.location("pathname").should("equal", "/signin");
-    
-  })
 
   it("should allow a visitor to sign-up, login, and logout", function () {
+   
     
     const userInfo = {
       firstName: "Bob",
@@ -21,17 +19,40 @@ describe('User Sign-up and Login', () => {
       username: "Katharina_Bernier",
       password: "s3cret",
     };
-    
-    // Login
+
+    // Then the user should be on sign in page
+    cy.location("pathname").should("equal", "/signin");
+    // When I enter my login credentials
     cy.login(userInfo.username, userInfo.password)
+
+    // Then I should be logged in and redirected to the dashboard
     cy.getBySel().should('exist')
 
-    // logout
+    // When I click on the logout button
     cy.getLogoutSel().should('contain', 'Logout').click()
     cy.location("pathname").should("eq", "/signin");
+
+    // And the user should be redirected to the login page
     cy.get('.makeStyles-form-56').should('be.visible')
 
+  });
+
+  it.only("should display login errors", function () {
+    // Given I am on the login page
+    cy.visit("/");
     
+    // When I enter my login credentials
+    cy.get("#username").type("Katharina_Bernier")
+    cy.get("#password").type("abcd").should("be.visible")
+
+    // When I click on the login button
+    cy.get('.MuiButton-label').click()
+
+    // And the user should be redirected to the login page
+    cy.errorMessage()
+      .should('be.visible')
+      .and("contain", "Username or password is invalid")
+     
   });
   
 })
